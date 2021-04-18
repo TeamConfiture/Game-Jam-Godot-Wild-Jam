c1298b90a6148b0e7e70bce4301dacd78e508c34
Mathis DANO <mathis85190@gmail.com>
2021-04-18 20:37:03 +0200

    First level works!

--- Fight 1.gd
+++ Fight 1.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-
-var enemyHP = 3400
+var MAX_ENEMY_HP = 1600
+var enemyHP = MAX_ENEMY_HP
 var ourHP = [70,70,70,70,70,70,70]
-var turn = 1
+var turn = 0
 var enemy_attack = false
 var j = 0
 var deaths = 0
@@ -28,6 +28,9 @@ func _process(delta):
 	elif _alive_count() == 0:
 		get_tree().change_scene("res://Loose.tscn")
 	if enemy_attack:
+		get_node("RollDice").disabled = true
+		get_node("pointer").position.x = 620
+		get_node("pointer").position.y = 170
 		if j > 50:
 			_enemy_attack()
 			j = 0
@@ -39,9 +42,9 @@ func _alive_count():
 
 func _change_life(nb: int):
 	get_node("Sprite").get_child(nb).get_child(0).region_enabled = true 
-	get_node("Sprite").get_child(nb).get_child(0).set_region_rect(Rect2((1-ourHP[nb]/70.0)*361,0,361,328))
+	get_node("Sprite").get_child(nb).get_child(0).set_region_rect(Rect2((1-ourHP[nb]/70.0)*361, 0, 361, 328))
 	get_node("Sprite").get_child(nb).get_child(0).set_offset(Vector2((1-ourHP[nb]/70.0)*361,0))
-	#print(ourHP[nb]/70.0)
+
 
 
 var posChatsX = [90, 230, 380, 515, 700, 830, 950]
@@ -50,66 +53,35 @@ func _attack():
 	get_node("Dice1").visible = true
 	get_node("EnemyDice").visible = false
 	
-	if turn >= 7:
-		turn = 1
+	if turn >= 6:
+		turn = 0
 		enemy_attack = true
-		get_node("RollDice").disabled = true
-		get_node("pointer").position.x = 400
-		get_node("pointer").position.y = 50
-		return
+		
 	
-	if ourHP[turn] == 0:
+	if ourHP[turn + 1] == 0:
 		turn += 1
 		_attack()
-		
-	get_node("pointer").position.x = posChatsX[turn]
+		return
+	
+	get_node("pointer").position.x = posChatsX[turn+1]
 	
 	var damage = roll_dice(20, "Dice1")
 	if (damage < enemyHP):
 		enemyHP -= damage
+		get_node("life").region_enabled = true
+		var w = 361
+		var h = 328
+		get_node("life").set_region_rect(Rect2((1-float(enemyHP)/float(MAX_ENEMY_HP))*w, 0, w, h))
+		get_node("life").set_offset(Vector2((1-float(enemyHP)/float(MAX_ENEMY_HP))*w, 0))
 	else:
 		enemyHP = 0
 	
 	turn += 1
-	
-	"""
-	if (get_node("pointer").position.x <= 1100):
-		var j = 0 if turn+1 == _alive_count() else turn+1
-		get_node("pointer").position.x += 150
-		while (ourHP[j] == 0):
-			print("<= 1100 Le chat  " + str(j) + " a " + str(ourHP[j]) + "HP")
-			get_node("pointer").position.x += 150
-			j += 1
-			if get_node("pointer").position.x >= 1100:
-				get_node("pointer").position.x += 100
-				j = 0
-	else:
-		var j = 0
-		get_node("pointer").position.x = 100
-		while (ourHP[j] == 0):
-			print("Le chat  " + str(j) + " a " + str(ourHP[j]) + "HP")
-			get_node("pointer").position.x += 150
-			j += 1
-	#get_node("RollDice").disabled = true
-	var damage = roll_dice(20, "Dice1")
-	if (damage < enemyHP):
-		enemyHP -= damage
-	else:
-		enemyHP = 0
-	turn += 1
-	if turn == _alive_count():
-		enemy_attack = true
-		get_node("RollDice").disabled = true
-		turn = 0
-		get_node("pointer").position.x = 400
-		get_node("pointer").position.y = 50
-	"""
-	
-	
+
 func _enemy_attack():
 	get_node("Dice1").visible = false
 	get_node("EnemyDice").visible = true
-	var damage = roll_dice(50, "EnemyDice") #/ _alive_count() # requires _check_deaths
+	var damage = roll_dice(30, "EnemyDice") #/ _alive_count() # requires _check_deaths
 	var nb = randi()%7
 	while (ourHP[nb] == 0):
 		nb = randi()%7
@@ -123,18 +95,8 @@ func _enemy_attack():
 		for i in range (7):
 			if ourHP[i] > 0:
 				get_node("pointer").position.x = posChatsX[i]
+				turn = i
 				break
-				print(posChatsX[i])
 		
 		get_node("pointer").position.y = 348.107
-		
-		#print("Le chat numéro" + str(nb) + " a " + str(ourHP[nb]) + "HP")
 		get_node("RollDice").disabled = false
-		"""var j = 0
-		get_node("pointer").position.x = 100
-		while (ourHP[j] == 0):
-			print("Le chat  " + str(j) + " a " + str(ourHP[j]) + "HP")
-			get_node("pointer").position.x += 150
-			j += 1
-		get_node("pointer").position.y = 348.107
-		"""
