2bca455ecbaa411564916b9aa028c6f9cdb7445c
Mathis DANO <mathis85190@gmail.com>
2021-04-18 19:12:59 +0200

    changed pointer sprite

--- Fight 1.gd
+++ Fight 1.gd
@@ -32,17 +32,19 @@ func _process(delta):
 			_enemy_attack()
 			j = 0
 		j+=1
-#	pass
+
+
 func _alive_count():
 	return len(get_node("Sprite").get_children()) - ourHP.count(0)
+
 func _change_life(nb: int):
 	get_node("Sprite").get_child(nb).get_child(0).region_enabled = true 
 	get_node("Sprite").get_child(nb).get_child(0).set_region_rect(Rect2((1-ourHP[nb]/70.0)*361,0,361,328))
 	get_node("Sprite").get_child(nb).get_child(0).set_offset(Vector2((1-ourHP[nb]/70.0)*361,0))
-	print(ourHP[nb]/70.0)
-	
+	#print(ourHP[nb]/70.0)
 
-var posChatsX = [100, 230, 380, 515, 700, 830, 950]
+
+var posChatsX = [90, 230, 380, 515, 700, 830, 950]
 
 func _attack():
 	get_node("Dice1").visible = true
